5c5bff2d0ae91a417589fdeaad3b5514905e712e
Dima Denysenko <coding@dimdnk.com>
2022-06-28 10:27:44 +0300

    feat: generate toString for entities

--- src/main/java/org/ujar/basics/restful/jwtauth/entity/Role.java
+++ src/main/java/org/ujar/basics/restful/jwtauth/entity/Role.java
@@ -1,13 +1,21 @@
 package org.ujar.basics.restful.jwtauth.entity;
 
+import java.util.Date;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 /**
  * Simple domain object that represents application user's role - ADMIN, USER, etc.
@@ -16,7 +24,7 @@ import lombok.Setter;
 @Table(name = "roles")
 @Getter
 @Setter
-public class Role extends BaseEntity {
+public class Role {
 
   @Column(name = "name")
   private String name;
@@ -24,10 +32,31 @@ public class Role extends BaseEntity {
   @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
   private List<User> users;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @CreatedDate
+  @Column(name = "created")
+  private Date created;
+
+  @LastModifiedDate
+  @Column(name = "updated")
+  private Date updated;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "status")
+  private Status status;
+
   @Override
   public String toString() {
     return "Role{" +
-           "id: " + super.getId() + ", " +
-           "name: " + name + "}";
+           "name='" + name + '\'' +
+           ", users=" + users +
+           ", id=" + id +
+           ", created=" + created +
+           ", updated=" + updated +
+           ", status=" + status +
+           '}';
   }
 }
