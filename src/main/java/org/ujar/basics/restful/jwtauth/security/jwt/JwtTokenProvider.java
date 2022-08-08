c8fe3fa332ea262a5806be718bed43fbba8605ec
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2022-08-08 19:08:48 +0300

    feat: use records for props
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- src/main/java/org/ujar/basics/restful/jwtauth/security/jwt/JwtTokenProvider.java
+++ src/main/java/org/ujar/basics/restful/jwtauth/security/jwt/JwtTokenProvider.java
@@ -33,8 +33,8 @@ public class JwtTokenProvider {
   public JwtTokenProvider(UserDetailsService userDetailsService,
                           JwtTokenProperties jwtTokenProperties) {
     this.userDetailsService = userDetailsService;
-    this.secret = Base64.getEncoder().encodeToString(jwtTokenProperties.secret().getBytes(StandardCharsets.UTF_8));
-    this.validityInMilliseconds = jwtTokenProperties.expiration();
+    this.secret = Base64.getEncoder().encodeToString(jwtTokenProperties.getSecret().getBytes(StandardCharsets.UTF_8));
+    this.validityInMilliseconds = jwtTokenProperties.getExpiration();
   }
 
   public String createToken(final String username, final List<Role> roles) {
