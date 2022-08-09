d94554b99790434fb31f7a30a2a2c47252764f80
Dima Denysenko <coding@dimdnk.com>
2022-08-09 09:58:08 +0300

    feat: improve encapsulation for services

--- src/main/java/org/ujar/basics/restful/jwtauth/security/JwtUserDetailsService.java
+++ src/main/java/org/ujar/basics/restful/jwtauth/security/JwtUserDetailsService.java
@@ -22,7 +22,7 @@ public class JwtUserDetailsService implements UserDetailsService {
   private final UserService userService;
 
   @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+  public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
     User user = userService.findByUsername(username);
 
     if (user == null) {
