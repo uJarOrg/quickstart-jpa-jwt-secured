ff9e286d84467a6fc6aaf7cb95fa300029a328d9
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2023-01-20 16:08:17 +0200

    chore: use shorter package naming
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

---
+++ src/main/java/org/ujar/bs/restful/jwtauth/entity/Status.java
@@ -0,0 +1,8 @@
+package org.ujar.bs.rst.jwtauth.entity;
+
+/**
+ * Enumeration that represents status of domain objects - ACTIVE, DELETED, etc.
+ */
+public enum Status {
+  ACTIVE, NOT_ACTIVE, DELETED
+}
---
+++ src/main/java/org/ujar/bs/restful/jwtauth/security/jwt/JwtAuthenticationException.java
@@ -0,0 +1,10 @@
+package org.ujar.bs.rst.jwtauth.security.jwt;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class JwtAuthenticationException extends AuthenticationException {
+
+  public JwtAuthenticationException(String msg) {
+    super(msg);
+  }
+}
