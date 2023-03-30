657b59a107a7e97f62e74d156cda4e0801b7cec1
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2023-03-30 17:59:26 +0300

    feat: migrate to Java 19, jakarta APIs, Spring Boot 3, Hibernate 6
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

---
+++ src/main/java/org/ujar/jwtsecured/JwtSecuredApplication.java
@@ -0,0 +1,13 @@
+package org.ujar.jwtsecured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecuredApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JwtSecuredApplication.class, args);
+  }
+
+}
---
+++ src/main/java/org/ujar/jwtsecured/entity/Role.java
@@ -0,0 +1,7 @@
+package org.ujar.jwtsecured.entity;
+
+public enum Role {
+
+  USER,
+  ADMIN
+}
