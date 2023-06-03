44d5cd3c3ff7cd571c16d3e9f230b94253132af0
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2023-06-03 18:45:46 +0300

    chore: enable reasonable defaults for observability components
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- src/main/java/org/ujar/jwtsecured/JwtSecuredApplication.java
+++ src/main/java/org/ujar/jwtsecured/JwtSecuredApplication.java
@@ -2,12 +2,15 @@ package org.ujar.jwtsecured;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class JwtSecuredApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(JwtSecuredApplication.class, args);
+    SpringApplication springApplication = new SpringApplication(JwtSecuredApplication.class);
+    springApplication.setApplicationStartup(new BufferingApplicationStartup(2048));
+    springApplication.run(args);
   }
 
 }
