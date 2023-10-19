63b4352980429e4b4250666776671d9783187af5
Dima Denysenko <coding@dimdnk.com>
2023-10-19 13:25:41 +0300

    feat: migrate to java 21

--- src/main/java/org/ujar/jwtsecured/service/JwtService.java
+++ src/main/java/org/ujar/jwtsecured/service/JwtService.java
@@ -38,10 +38,10 @@ public class JwtService {
   ) {
     return Jwts
         .builder()
-        .setClaims(extraClaims)
-        .setSubject(userDetails.getUsername())
-        .setIssuedAt(new Date(System.currentTimeMillis()))
-        .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+        .claims(extraClaims)
+        .subject(userDetails.getUsername())
+        .issuedAt(new Date(System.currentTimeMillis()))
+        .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
         .signWith(getSignInKey(), SignatureAlgorithm.HS256)
         .compact();
   }
@@ -60,12 +60,11 @@ public class JwtService {
   }
 
   private Claims extractAllClaims(String token) {
-    return Jwts
-        .parserBuilder()
+    return Jwts.parser()
         .setSigningKey(getSignInKey())
         .build()
-        .parseClaimsJws(token)
-        .getBody();
+        .parseSignedClaims(token)
+        .getPayload();
   }
 
   private Key getSignInKey() {
