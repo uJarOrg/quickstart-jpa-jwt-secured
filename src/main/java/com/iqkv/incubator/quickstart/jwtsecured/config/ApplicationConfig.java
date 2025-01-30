7a23dd0ee20ff1f80941c1667b59ce362e3a06a5
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2025-01-30 19:09:59 +0200

    chore: update streamline version and copyright headers

--- src/main/java/com/iqkv/incubator/quickstart/jwtsecured/config/ApplicationConfig.java
+++ src/main/java/com/iqkv/incubator/quickstart/jwtsecured/config/ApplicationConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 IQKV Team.
+ * Copyright 2025 IQKV Team.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,7 +33,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
-@OpenAPIDefinition(info = @Info(title = "JWT-based Spring security REST API", version = "24.0.0"))
+@OpenAPIDefinition(info = @Info(title = "JWT-based Spring security REST API", version = "25.0.0"))
 public class ApplicationConfig {
 
   private final UserRepository repository;
